cf0cc8ee9af437e0e848d2fa973a5d223f6ecbc1
DavidPalaciosM <63470827+DavidPalaciosM@users.noreply.github.com>
2022-06-09 06:59:27 -0400

    Update FuelModel.h
    Add Fuel Model KITRAL

--- cell2fire/Cell2FireC/FuelModel.h
+++ cell2fire/Cell2FireC/FuelModel.h
@@ -1,134 +1,100 @@
-/*  fbp.h  -- header file for fbp program  bmw
-  function prototypes and structures for fbp.c prog
-   nov/92
-  modified for version 4.3 of the code
-   Aug/2005 bmw
-  modified for version 4.5 of the code
-   Nov/2007 bmw
-   change of name for 5.0
+#ifndef FUELMODELKITRAL
+#define FUELMODELKITRAL
+
+#include <iostream>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <string.h>
+#include <unordered_map>
+#include <string>
+#include <vector>
+
+/*
+*  Input, Fire, and output structures
 */
 
-#if !defined(_FBPHEAD)
-#define _FBPHEAD
-
-#include <stdio.h>
-
- typedef struct
+typedef struct
    { char fueltype[4];
-     float ffmc,ws,gfl,bui,lat,lon;
-     int time,pattern,mon,jd,jd_min,waz,ps,saz,pc,
-          pdf,cur,elev,hour,hourly;
+     float ws, saz, cur, ps, cbh, ccf, cbd, elev , tmp, rh;
+     int waz, scen, nftype;
+     float factor_cbd, factor_ccf, factor_ros10, factor_actv ;
+	 bool cros, verbose;
    } inputs;
 
+
 typedef struct
-  { char fueltype[4];
-    float q,bui0,cbh,cfl;
-    double a,b,c;
+  { char fueltype[4] ;
+    float fmc, cbh, fl ,h ;    // hros coef
+    // Kitral:     l1 = 1.466, l2 = -0.01488
+    // BehavePlus: l1 = 19.65, l2 = 0.00216
+    // FARSITE:    l1 = 1.981, l2 = -0.02704
+    // FBP System: l1 = 3.061, l2 = 0.02656
+	int nftype; 
   } fuel_coefs;
   
- typedef struct
-  { float ros,dist,rost,cfb,fc,cfc,time,rss,isi;
-    char fd;
-    double fi;
-  }fire_struc;
-
- typedef struct
-  {  float hffmc,sfc,csi,rso,fmc,sfi,rss,isi,be,sf,raz,wsv,ff;
-     int jd_min,jd;
+  typedef struct
+  { float fl, fh, a, b, c, rss, angle, byram;
      char covertype;
+	 bool cros;
   } main_outs;
 
- typedef struct
-  { float lb,area,perm,pgr,lbt;
+typedef struct
+  { float ros, rss;
+    double fi;
+  } fire_struc;
+
+  typedef struct
+  { float lb, area, perm, pgr, lbt;
   } snd_outs;
 
 
+ /*
+		Functions
+ */
+// Initialize coefficients
+void setup_const();
 
-float grass(fuel_coefs *ptr,float cur,float isi,float *mu);
+ // Calculate flank fire 
+float flankfire_ros(float ros, float bros, float lb) ;
 
-float mixed_wood(fuel_coefs *ptr,float isi, float *mult,int pc);
+// Calculate rate of spread
+float rate_of_spread(inputs *data, fuel_coefs *ptr, main_outs *at) ;
 
- float ISF_mixedwood(fuel_coefs *ptr, float isz, int pc, float sf);
+// Length-to-Breadth ratio
+float l_to_b(float ws);
 
- float ISF_deadfir(fuel_coefs *ptr, float isz, int pdf, float sf);
+// BROS
+float backfire_ros(main_outs *at, snd_outs *sec) ;
 
-float dead_fir(fuel_coefs *ptr,int pdf,float isi,float *mult);
+// Flame length [m])
+float flame_length(inputs *data, fuel_coefs *ptr);
 
-float D2_ROS(fuel_coefs *ptr, float isi, float bui, float *mu);
+// Angle of the flame w.r.t. horizontal surface (Putnam's)
+float angleFL(inputs *data, fuel_coefs *ptr) ;
 
-float conifer(fuel_coefs *ptr,float isi,float *mult);
+// Transformation from FL to FH using angle 
+float flame_height(inputs* data, fuel_coefs* ptr);
 
-float bui_effect(fuel_coefs *ptr,main_outs *at, float bui);
+//byram intensity
+float byram_intensity(inputs* data, fuel_coefs* ptr);
 
-float ros_calc(inputs *inp, fuel_coefs *ptr, float isi, float *mu);
+// Type of fire (Crown = 1, SROS = 0)
+bool fire_type(inputs *data, fuel_coefs *ptr) ;
 
-float rate_of_spread(inputs *inp,fuel_coefs *ptr, main_outs *at);
+// CROS adjustements
+float rate_of_spread10(inputs *data) ;
 
-float slope_effect(inputs *inp, fuel_coefs *ptr, main_outs *at,float isi);
+// Back fire with CROS 
+float backfire_ros10(fire_struc *hptr, snd_outs *sec) ;
 
-float surf_fuel_consump(inputs *inp);
+// Slope effect
+float slope_effect(inputs * data);
 
-float fire_intensity(float fc,float ros);
+// Main function to populate spread outputs based on inputs provided from main class
+void calculate(inputs *data,  fuel_coefs * ptr, main_outs *at, snd_outs *sec, fire_struc *hptr, fire_struc *fptr,fire_struc *bptr);
 
-void setup_const(fuel_coefs *ptr);
-
-char get_fueltype_number(fuel_coefs **ptr,char ft[4]);
-
-float foliar_moisture(inputs *inp, main_outs *at);
-
-float crit_surf_intensity(fuel_coefs *ptr,float fmc);
-
-float critical_ros(char ft[3],float sfc,float csi);
-
-char fire_type(float csi,float sfi);
-
-float crown_frac_burn(float ros,float rso);
-
-char fire_description(float cfb);
-
-float final_ros(inputs *inp, float fmc,float isi,float cfb,float rss);
-
-float crown_consump(inputs *inp, fuel_coefs *ptr, float cfb);
-
-float foliar_mois_effect(float isi,float fmc);
-
-float l_to_b(char ft[3],float ws);
-
-float ffmc_effect(float ffmc);
-
-float backfire_ros(inputs *inp,fuel_coefs *ptr, main_outs *at,float bisi);
-
-float backfire_isi(main_outs *at);
-
-float area(float dt,float df);
-
-float perimeter(fire_struc *h,fire_struc *b,snd_outs *sec,float lb);
-
-float acceleration(inputs *inp, float cfb);
-
-float flankfire_ros(float ros,float bros,float lb);
-
-float spread_distance(inputs *inp, fire_struc *ptr, float accn);
-
-float flank_spread_distance(inputs *inp,fire_struc *ptr, snd_outs *sec,
-      float hrost,float brost,float hd, float bd,float lb,float a);
-
-int time_to_crown(float ros,float rso,float a);
-
-float fire_behaviour(inputs *inp,fuel_coefs *ptr,main_outs *at,
-                                                    fire_struc *f);
-float flank_fire_behaviour(inputs *inp,fuel_coefs *ptr,main_outs *at,
-                             fire_struc *f);
-
-void set_all(fire_struc *ptr, int time);
-
-void calculate(inputs *data,fuel_coefs *ptr,main_outs *at,
-      snd_outs *sec,fire_struc *hptr,fire_struc *fptr,fire_struc *bptr);
-
-void zero_main(main_outs *m);
-
-void zero_sec (snd_outs *s);
-
-void zero_fire(fire_struc *a);
+void determine_destiny_metrics(inputs* data, fuel_coefs* ptr, main_outs* at);
 
 #endif
